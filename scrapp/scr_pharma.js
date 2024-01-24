303d9bfbb3b891ed37a84663f69a5f69d13d49e5
Stupnikjs <n.boudier.ph@gmail.com>
2024-01-24 14:33:33 +0100

    ok

--- scrapp/scr_pharma.js
+++ scrapp/scr_pharma.js
@@ -8,35 +8,32 @@ async function pharma3000(url){
 
     const browser = await puppeteer.launch({
         //executablePath: '/usr/bin/chromium-browser', 
-        headless: true,
+        headless: "new",
       })
   
    let page = await browser.newPage(); 
 
    await page.goto(url)
-   // let paginator = await page.$$('li.ajax_block_product.col-xs-6.col-sm-4.col-md-4 slide-list')
+   let paginator = await page.$$('.pagination > li ')
+   paginator.pop()
+   let anteLast = paginator.pop()
+
+   let totalPages = await anteLast.evaluate(el => el.textContent)
 
    await oneScrappingProcess(page)
-/*
+
    let urls = [] 
    let finalResult = []
-   for (let i = 1; i < paginator.length; i++){
-            
-         urls.push(await paginator[i]?.evaluate(el => el.href))
-            
+   for (let i = 2; i < totalPages - 1; i++){
+        let url_to = `https://www.pharmacie-cap3000.com/11-medicaments#/page-${i}`
+        await page.goto(url_to)   
+        let result = await oneScrappingProcess(page);
+        finalResult = finalResult.concat(result, finalResult)    
         }
-    
-    for (let url of urls){
-
-    await page.goto(url)
-    let result = await oneScrappingProcess(page);
-    finalResult = finalResult.concat(result, finalResult)
-    }  
 
    let resultJson = JSON.stringify(finalResult)
    let jsonFileName = new Date().getTime() + 'races.json'
    fs.writeFileSync(jsonFileName, resultJson)
-   */
    browser.close() 
 }
 
@@ -65,9 +62,26 @@ async function processElement(element){
         let pre = await element.waitForSelector('pre')
 
         let bstext = await pre.evaluate(el => el.textContent)
-        let index = bstext.indexOf('["cip13"]')
-        let cip13 = bstext.substring(index - 17, index - 3)
-        console.log(cip13)
+        let index_cip13 = bstext.indexOf('string(13)')
+        let cip13 = bstext.substring(index_cip13 + 12 , index_cip13 + 25)
+        pharmaObj['cip13'] = cip13
+
+        let index_cip7 = bstext.indexOf('string(7)')
+        let cip7 = bstext.substring(index_cip7 + 11 , index_cip7 + 18)
+        pharmaObj['cip7'] = cip7
+
+        let index_update = bstext.indexOf('string(19)')
+        let update = bstext.substring(index_update + 12 , index_update + 25)
+        pharmaObj['update'] = update
+
+        let title = await element.waitForSelector(' .prod-info > .prod-title')
+
+        pharmaObj['name'] = await title.evaluate(el => el.textContent)
+
+
+        console.log(pharmaObj)
+        
+
         
         
         
