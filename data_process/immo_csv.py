56331ccfc271a6c9dc281d02cb45026d674ed4f6
Stupnikjs <n.boudier.ph@gmail.com>
2024-02-03 22:09:07 +0100

    ok

--- data_process/immo_csv.py
+++ data_process/immo_csv.py
@@ -1,16 +1,19 @@
 import pandas as pd 
 
-def my_split(x): 
-    return x.split(" ")
-
-
-
-
 df = pd.read_csv('output.csv')
 
-df['postal_code'] = df['adress'].apply(my_split)
+
+
+df['price_per_m'] = df['price_per_m'].str[:-4]
+df['price'] = df['price'].str[:-2]
+df['postal_code'] = df['adress'].str[:5]
+df['location'] = df['adress'].str[5:]
+df['type'] = df['title'].str.split().str[0]
+df['area'] = df['title'].str.split().str[-2]
+df['room'] = df['title'].str.split().str[-4]
+df = df.drop(columns=['title', 'adress'])
 
 
 
+df.to_csv('cleaned.csv')
 
-print(df['postal_code'])
