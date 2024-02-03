1a4a4445105ebec762c4ec699c22e56b0c99a8da
Stupnikjs <n.boudier.ph@gmail.com>
2024-02-03 09:41:30 +0100

    ok

--- data_process/immo_csv.py
+++ data_process/immo_csv.py
@@ -1,11 +1,16 @@
 import pandas as pd 
 
 def my_split(x): 
-    return x.split(' ')[0]
+    return x.split(" ")
+
+
 
 
 df = pd.read_csv('output.csv')
 
 df['postal_code'] = df['adress'].apply(my_split)
 
-print(df.head())
+
+
+
+print(df['postal_code'])
